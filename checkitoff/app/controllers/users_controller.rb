a5b62015f9d6c8fecda6662eb118069e59d232d7
Randyr902 <rdj7890@gmail.com>
2015-08-12 13:56:18 -0700

    added users controller and updated user model

--- checkitoff/app/controllers/users_controller.rb
+++ checkitoff/app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
-  before_filter :authenticate_user!
+  before_action :authenticate_user!, except:[:show]
+
+  def update
+    if current_user.update_attributes(user_params)
+      flash[:notice] = "User information was updated."
+      redirect_to edit_user_registration_path
+    else
+      flash[:error] = "Invalid user information."
+      redirect_to edit_user_registration_path
+  end
 
   def show
     @user = User.find(params[:id])
