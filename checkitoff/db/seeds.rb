348e7e2a24eb5b5484b3ac35e73dc2c70335d1b5
Randyr902 <rdj7890@gmail.com>
2015-08-20 06:28:52 -0700

    checkpoint-seeding-data in progress

--- checkitoff/db/seeds.rb
+++ checkitoff/db/seeds.rb
@@ -1,19 +1,3 @@
 require 'faker'
 
- 50.times do
-   Check_list.create!(
-     title:  Faker::Lorem.sentence,
-     body:   Faker::Lorem.paragraph
-   )
- end
- check_lists = Check_list.all
-
- 100.times do
-   Item.create!(
-     body: Faker::Lorem.paragraph
-   )
- end
-
- puts "Seed finished"
- puts "#{Check_list.count} check lists created"
- puts "#{Item.count} items created"
+ 
